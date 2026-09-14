f1cb098d923fc269471fbae318054ce8f163382d
Prince gupta <26LBCO3004@culkomail.in>
2026-09-14 21:28:10 +0530

    feat: add tauri v2 windows desktop app setup and github actions build workflow

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,20 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+
+fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.set_focus();
+            }
+        }))
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(tauri_plugin_autostart::init(
+            tauri_plugin_autostart::MacosLauncher::LaunchAgent,
+            Some(vec![]),
+        ))
+        .run(tauri::generate_context!())
+        .expect("error while running XYro - gym os");
+}
